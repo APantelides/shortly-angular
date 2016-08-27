7c8d7f7dfcef8bee3de629f2c147ca9a62707879
apantelides <alex.pantelides@gmail.com>
2016-08-26 21:24:25 -0700

    Pass alltests, implement html for links and shorten, implement ordering by views, implement routes, implement authentication

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,14 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
+  $scope.link = {};
+  $scope.addLink = function () {
+    console.log($scope.link);
+    Links.addOne($scope.link).then(function() {
+      $scope.link.url = '';
+    });
+  };
+  $scope.logoff = function () {
+    Auth.signout();
+  };
 });
