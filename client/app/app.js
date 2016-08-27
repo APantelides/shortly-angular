f23ea2729c6e48348000296fe0665510ce6b8f7b
Alex Pantelides <alex.pantelides@gmail.com>
2016-08-27 10:31:19 -0700

    lex attempt

--- client/app/app.js
+++ client/app/app.js
@@ -15,6 +15,10 @@ angular.module('shortly', [
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
     })
+    .when('/', {
+      templateUrl: 'app/links/links.html',
+      controller: 'AuthController'
+    });
     // Your code here
 
     // We add our $httpInterceptor into the array
